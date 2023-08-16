a1d6aa4362c64fb63e42e69ab1daca05eb23bee0
naveen_hider <117838700+naveenGlover@users.noreply.github.com>
2023-08-16 17:45:08 +0530

    Delete eventsBooking directory

--- eventsBooking/index.js
+++
@@ -1,8 +0,0 @@
-const login=()=>{
-    let loggedAlready=localStorage.getItem("currentUser");
-    if(loggedAlready!==null){
-        window.location='../pages/userPage.html';
-    } else{
-        window.location='../pages/userLogin.html';
-    }
-}
